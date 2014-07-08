59d68e6d6859ffbe0cee30431c4de6f4ca9f0b7e
Justin Plock <justin@plock.net>
2014-07-08 10:36:13 -0400

    Use a more generic default namespace

--- src/main/java/com/ge/snowizard/discovery/DiscoveryFactory.java
+++ src/main/java/com/ge/snowizard/discovery/DiscoveryFactory.java
@@ -61,7 +61,7 @@ public class DiscoveryFactory {
     private String listenAddress = "127.0.0.1";
 
     @NotEmpty
-    private String namespace = "ge";
+    private String namespace = "snowizard";
 
     @NotEmpty
     private String basePath = "service";
